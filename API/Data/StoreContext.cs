0a24b688a588f5cd655c92d89fa966d13b50391f
mariia.vasylchenko <choicl05@gmail.com>
2022-09-06 19:02:02 +0300

    Initial commit

---
+++ API/Data/StoreContext.cs
@@ -0,0 +1,18 @@
+using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data;
+
+public class StoreContext: DbContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Data Source=(local);Initial Catalog=EStoreDb;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+    }
+    public StoreContext(DbContextOptions options) : base(options)
+    {
+        Database.EnsureCreated();
+    }
+
+    public DbSet<Product> Products { get; set; }
+}
