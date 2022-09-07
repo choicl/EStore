49734b066ce8e5c23ae91b21d2d3f44a2124b68a
mariia.vasylchenko <choicl05@gmail.com>
2022-09-07 17:24:09 +0300

    Created client app

---
+++ API/Program.cs
@@ -0,0 +1,52 @@
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<StoreContext>(opt =>
+{
+    opt.UseSqlServer("Data Source=(local);Initial Catalog=EStoreDb;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+});
+builder.Services.AddCors();
+ 
+var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var context = scope.ServiceProvider.GetRequiredService<StoreContext>();
+    var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
+    try
+    {
+        context.Database.Migrate();
+        DbInitializer.Initialize(context);
+    }
+    catch (Exception e)
+    {
+        logger.LogError(e, "Problem migrating data");
+    }
+}
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseCors(opt =>
+{
+    opt.AllowAnyHeader().AllowAnyOrigin().AllowAnyMethod();
+});
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
