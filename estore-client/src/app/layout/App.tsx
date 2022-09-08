6e3595a69ffa308a794bba549f2524731ea7fd6a
mariia.vasylchenko <choicl05@gmail.com>
2022-09-08 11:24:56 +0300

    Added some pages and routing

--- estore-client/src/app/layout/App.tsx
+++ estore-client/src/app/layout/App.tsx
@@ -2,6 +2,11 @@ import Catalog from '../features/catalog/Catalog';
 import { Container, createTheme, CssBaseline, ThemeProvider } from '@mui/material';
 import Header from './Header';
 import { useState } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import HomePage from '../features/home/HomePage';
+import ProductDetails from '../features/catalog/ProductDetails';
+import AboutPage from '../features/about/AboutPage';
+import ContactPage from '../features/contact/ContactPage';
 
 function App() {
   const [darkMode, setDarkMode] = useState(false);
@@ -24,7 +29,13 @@ function handleThemeChange(){
       <CssBaseline/>
         <Header darkMode={darkMode} handleThemeChange={handleThemeChange} />
           <Container>
-              <Catalog/>
+            <Routes>
+              <Route path='/' element={<HomePage/>}/>
+              <Route path='/catalog' element={<Catalog/>}/>
+              <Route path='/catalog/:id' element={<ProductDetails/>}/>
+              <Route path='/about' element={<AboutPage/>}/>
+              <Route path='/contact' element={<ContactPage/>}/>
+            </Routes>
           </Container>
     </ThemeProvider>
   );
